65787e04eb2e87b5a8c21cace254c43bc1610602
chiomadans1759 <chiomadans@gmail.com>
2019-07-01 22:32:31 +0100

    #166998258 Created the User Login Endpoint

--- server/Routes/routes.js
+++ server/Routes/routes.js
@@ -1,7 +1,8 @@
 import UserController from '../Controllers/userControllers';
-import auth from '../Middleware/auth';
+// import auth from '../Middleware/auth';
 
 /* All Router Here */
 export default (server) => {
   server.post('/api/v1/users', UserController.addUser);
+  server.post('/api/v1/login', UserController.loginUser);
 };
