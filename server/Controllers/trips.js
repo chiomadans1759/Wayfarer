33137552c99a08943324a329466e75f43a6ff2a3
chiomadans1759 <chiomadans@gmail.com>
2019-07-16 12:58:41 +0100

    Updating repo

--- server/Controllers/trips.js
+++ server/Controllers/trips.js
@@ -39,6 +39,7 @@ export default class TripsController {
         },
       });
     } catch (error) {
+      console.log(error);
       return res.status(500).json({
         status: 'error',
         error: 'Server Error',
